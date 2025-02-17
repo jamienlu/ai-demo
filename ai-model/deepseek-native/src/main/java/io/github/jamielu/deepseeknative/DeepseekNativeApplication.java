b11dc1ea60e549637e220a2929c8865449a88089
jamie <77570648@qq.com>
2025-02-17 21:53:27 +0800

    native-deepseek test

---
+++ ai-model/deepseek-native/src/main/java/io/github/jamielu/deepseeknative/DeepseekNativeApplication.java
@@ -0,0 +1,13 @@
+package io.github.jamielu.deepseeknative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepseekNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepseekNativeApplication.class, args);
+    }
+
+}
